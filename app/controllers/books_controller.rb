5e3ae2a537426befc70484b7652b262760efa262
nozakirio <wada.rio1001@gmail.com>
2020-12-08 06:10:06 +0000

    add search by titile

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,15 @@
 class BooksController < ApplicationController
   def search
+    @keyword = params[:keyword]
+    if params[:keyword]
+      @books = RakutenWebService::Books::Book.search(title: @keyword)
+    end
   end
 
   def create
   end
-  
+
   def show
   end
+
 end
